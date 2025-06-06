7682e9facdf657c7c43256594e2495414339d22e
zearanha <JAranha21@gmail.com>
2025-06-06 15:48:21 -0300

    Finalizando pagina de consulta

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'
 import {MatInputModule} from '@angular/material/input'
 import { MatCardModule } from '@angular/material/card'
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -19,13 +20,15 @@ import { Cliente } from '../cadastro/cliente';
     MatButtonModule,
     FlexLayoutModule,
     FormsModule,
+    CommonModule,
   ],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit {
   
   listaClientes: Cliente[] = [];
+  colunasTable: string[] = ["id", "nome", "cpf", "dataNascimento", "email"]
   
   constructor(private service: ClientService){
 
